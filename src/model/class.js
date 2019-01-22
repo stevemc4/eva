374e095ff51d7243cde43c5dc4ee1f3d7a8019e0
Dhika Rizky <deka_r@outlook.com>
2019-01-22 09:08:25 +0700

    Added class model

---
+++ src/model/class.js
@@ -0,0 +1,60 @@
+import db from './db'
+
+/**
+ * Classes Class
+ */
+class Class{
+    /**
+     * Create new Class instance
+     * @param {object} data Initial data
+     * @param {string} data.class Class name
+     */
+    constructor(data){
+        this.id = data.id || undefined
+        this.className = data.class
+    }
+
+    /**
+     * Get instances from database
+     * @param {string} [className] Class name to find
+     * @returns {(Class | Class[])} Class instance
+     */
+    static async get(className){
+        if(className != undefined){
+            var c = await db('classes').select('class').where('class', className)
+            return new Class(c[0])
+        }
+        else{
+            var temp = []
+            var c = await db('classes').select('class').where('class', className)
+            for(let item of c)
+            {
+                temp.push(new Class(item))
+            }
+            return temp
+        }
+    }
+
+    /**
+     * Creates new Class in database or updates the record if exists
+     * @returns {boolean} True if action is successful
+     */
+    async save(){
+        try{
+            if(this.id == undefined){
+                db('classes').insert({class: this.className})
+            }
+            else
+            {
+                db('classes').update({class: this.className}).where('id', this.id)
+            }
+            return true
+        }
+        catch(e){
+            return false
+        }
+    }
+}
+
+export default Class
+
