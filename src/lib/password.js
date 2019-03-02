afd119013f7e48761bf15038467284af1eb0d6e7
Dhika Rizky <deka_r@outlook.com>
2019-03-02 20:41:12 +0700

    Minor changes

--- src/lib/password.js
+++ src/lib/password.js
@@ -9,14 +9,13 @@ class Password{
      * @param {string} password Plain text password
      */
     constructor(password){
-        const cipher = crypto.createCipheriv('aes-256-gcm', new Buffer(process.env.CRYPTO), 'as')
+        const cipher = crypto.createCipheriv('aes-256-gcm', Buffer.from(process.env.CRYPTO), 'as')
         const hash = crypto.createHash('sha256')
         this.value = hash.update(cipher.update(password).toString('hex'))
             .digest().toString('hex')
     }
     compareTo(otherPassword){
-        let o = new Password(otherPassword)
-        if(o.value == this.value)
+        if(otherPassword == this.value)
             return true
         else return false
     }
