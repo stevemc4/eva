cd85458f8389ff3e29d74ba6918ae5fd10b5b450
Dhika Rizky <deka_r@outlook.com>
2019-03-09 18:11:40 +0700

    Fixed bug in VoteSignature class

--- src/lib/voteSignature.js
+++ src/lib/voteSignature.js
@@ -11,7 +11,7 @@ class VoteSignature{
         let cipher = Buffer.from(parsedSignature[0], 'hex')
         let key = Buffer.from(parsedSignature[1], 'hex')
         let iv = Buffer.from(parsedSignature[2], 'hex')
-        let length = Number.parseInt(parsedSignature[4])
+        let length = Number.parseInt(parsedSignature[3])
         let decipherer = crypto.createDecipheriv('aes-256-gcm', key ,iv)
         let content = Buffer.from(decipherer.update(cipher)).toString('utf8', 0, length)
         return JSON.parse(content)
