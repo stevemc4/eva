bc08c33f6edbef35459eedcb07a8f9336e1cab52
Dhika Rizky <deka_r@outlook.com>
2019-03-09 17:51:01 +0700

    Added voted signature validation class

---
+++ src/lib/voteSignature.js
@@ -0,0 +1,21 @@
+import crypto from 'crypto'
+
+class VoteSignature{
+    /**
+     * Decrypts a vote's signature
+     * @param {string} signature Vote signature
+     * @returns {object} Parsed signature
+     */
+    static decrypt(signature){
+        let parsedSignature = signature.split(/\./g)
+        let cipher = Buffer.from(parsedSignature[0], 'hex')
+        let key = Buffer.from(parsedSignature[1], 'hex')
+        let iv = Buffer.from(parsedSignature[2], 'hex')
+        let length = Number.parseInt(parsedSignature[4])
+        let decipherer = crypto.createDecipheriv('aes-256-gcm', key ,iv)
+        let content = Buffer.from(decipherer.update(cipher)).toString('utf8', 0, length)
+        return JSON.parse(content)
+    }
+}
+
+export default VoteSignature
